fcdfd84d046993d2530d8d60e89ae070e144b0e1
xu ding <thedilab@gmail.com>
2017-01-26 22:37:42 +0800

    implement finder on collection

--- src/Collection.php
+++ src/Collection.php
@@ -2,6 +2,7 @@
 
 
 namespace Dilab\CakeMongo;
+
 use Cake\Datasource\EntityInterface;
 use Cake\Datasource\RepositoryInterface;
 
@@ -15,6 +16,62 @@ use Cake\Datasource\RepositoryInterface;
  */
 class Collection implements RepositoryInterface
 {
+
+    /**
+     * Calls a finder method directly and applies it to the passed query
+     *
+     * @param string $type name of the finder to be called
+     * @param \Dilab\CakeMongo\Query $query The query object to apply the finder options to
+     * @param array $options List of options to pass to the finder
+     * @return \Dilab\CakeMongo\Query
+     * @throws \BadMethodCallException
+     */
+    public function callFinder($type, Query $query, array $options = [])
+    {
+        $query->applyOptions($options);
+        $options = $query->getOptions();
+        $finder = 'find' . ucfirst($type);
+        if (method_exists($this, $finder)) {
+            return $this->{$finder}($query, $options);
+        }
+
+        throw new \BadMethodCallException(
+            sprintf('Unknown finder method "%s"', $type)
+        );
+    }
+
+    /**
+     * Creates a new Query for this repository and applies some defaults based on the
+     * type of search that was selected.
+     *
+     * ### Model.beforeFind event
+     *
+     * Each find() will trigger a `Model.beforeFind` event for all attached
+     * listeners. Any listener can set a valid result set using $query
+     *
+     * @param string $type the type of query to perform
+     * @param array $options An array that will be passed to Query::applyOptions
+     * @return \Dilab\CakeMongo\Query
+     */
+    public function find($type = 'all', $options = [])
+    {
+        $query = $this->query();
+
+        return $this->callFinder($type, $query, $options);
+    }
+
+    /**
+     * Returns the query as passed
+     *
+     * @param \Dilab\CakeMongo\Query $query An MongoDB query object
+     * @param array $options An array of options to be used for query logic
+     * @return \Dilab\CakeMongo\Query
+     */
+    public function findAll(Query $query, array $options = [])
+    {
+        return $query;
+    }
+
     public function alias($alias = null)
     {
         // TODO: Implement alias() method.
@@ -25,11 +82,6 @@ class Collection implements RepositoryInterface
         // TODO: Implement hasField() method.
     }
 
-    public function find($type = 'all', $options = [])
-    {
-
-    }
-
     public function get($primaryKey, $options = [])
     {
         // TODO: Implement get() method.
@@ -37,7 +89,7 @@ class Collection implements RepositoryInterface
 
     public function query()
     {
-        // TODO: Implement query() method.
+        return new Query($this);
     }
 
     public function updateAll($fields, $conditions)
---
+++ tests/TestCase/CollectionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Dilab\CakeMongo;
+
+
+use Cake\Datasource\ConnectionManager;
+use Cake\TestSuite\TestCase;
+
+class CollectionTest extends TestCase
+{
+    /**
+     * @var Collection
+     */
+    public $collection;
+
+    public $fixtures = ['plugin.cake/elastic_search.articles'];
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->collection = new Collection([
+            'name' => 'articles',
+            'connection' => ConnectionManager::get('test')
+        ]);
+    }
+
+    /**
+     * Tests that calling find will return a query object
+     *
+     * @return void
+     */
+    public function testFindAll()
+    {
+        $query = $this->collection->find('all');
+        $this->assertInstanceOf('Dilab\CakeMongo\Query', $query);
+        $this->assertSame($this->collection, $query->repository());
+    }
+}
