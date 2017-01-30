0966940f1d9c6433113b30bab80c1156315be197
xu ding <thedilab@gmail.com>
2017-01-30 23:38:20 +0800

    work on connection class

---
+++ tests/TestCase/Datasource/ConnectionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+use Cake\TestSuite\TestCase;
+
+class ConnectionTest extends TestCase
+{
+    public $fixtures = [
+
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testDummy()
+    {
+        $this->markTestSkipped();
+    }
+}
